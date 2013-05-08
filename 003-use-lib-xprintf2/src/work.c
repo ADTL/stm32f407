e2771d2bf0bfaf6e5c7bade533ab83fa634405c4
Boris Vinogradov <no111u3@gmail.com>
2013-05-08 09:14:26 +0700

    add 002-use-lib-xprintf(2) projects

---
+++ 003-use-lib-xprintf2/src/work.c
@@ -0,0 +1,13 @@
+/*-----------------------------------------------------------------------------
+* Author: Boris Vinogradov(nis) : no111u3@gmail.com
+* File: work.c
+* Type: C code file
+* Project: STM32F4D
+* 2013
+*-----------------------------------------------------------------------------*/
+/* includes */
+/* main work function */
+void work(void) {
+    /* infinity loop */
+    while (1);
+}
